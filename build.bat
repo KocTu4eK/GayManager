ec67149302fe70ea12eae1fd44d3f88ec9ca320c
KocTu4eK <koctu4ek.koctu4ek@gmail.com>
2024-01-15 23:53:46 +0600

    Fixed path to icon

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 mkdir output
 
 pyrcc5 -o source/resources.py resources.qrs
-nuitka --windows-icon-from-ico=icon.ico --windows-disable-console --output-dir=output -o output/GayManager --follow-imports source/main.py
+nuitka --windows-icon-from-ico=assets/icon.ico --windows-disable-console --output-dir=output -o output/GayManager --follow-imports source/main.py
